83bfffd895c129d324d4c391e41c4bc1c66ed3f4
RihardXXX <yusupa.akaeva@yandex.ru>
2021-01-19 09:49:50 +0300

    create article 1

---
+++ src/views/CreateArticle.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Create article</h1>
+        <mvArticleForm
+            :initialValues="initialValues"
+            :errors="validationErrors"
+            :isSubmitting="isSubmitting"
+            @articleSubmit="onSubmit"
+        />
+    </div>
+</template>
+
+<script>
+import MvArticleForm from '@/components/ArticleForm'
+
+export default {
+    name: 'MvCreateArticle',
+    data() {
+        return {
+            initialValues: {
+                title: '',
+                description: '',
+                body: '',
+                tagList: []
+            },
+            validationErrors: null
+        }
+    },
+    components: {
+        MvArticleForm
+    },
+    methods: {
+        onSubmit(data) {
+            console.log('onSubmit in create Article', data)
+        }
+    }
+}
+</script>
